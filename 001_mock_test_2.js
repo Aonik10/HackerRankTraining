b70ece419295fd835cb6f6c291cc7221333a2403
Emiliano <emp10999@gmail.com>
2023-04-08 23:59:09 -0300

    mock test 2 desc added

--- 001_mock_test_2.js
+++ 001_mock_test_2.js
@@ -1,9 +1,20 @@
 /*
-Pseudo code,
-i = 0 quien puede estar en la posicion de i = 0?
-pues todos los i=0 o i=(row.length - 1 - i) de cada fila o columna
-i = 1, quien puede estar en la posicion i = 1?
-pues todos los i=1 o i=(row.length - 1 - i) de cada fila o columna
+Sean invented a game involving a 2n x 2n matrix where each cell of the matrix contains an integer. He
+can reverse any of its rows or columns any number of times. The goal of the game is to maximize the sum
+of the elements in the n x n submatrix located in the upper-left quadrant of the matrix.
+Given the initial configurations for Q matrices, help Sean reverse the rows and columns of each matrix in the
+best possible way so that the sum of the elements in the matrix's upper-left quadrant is maximal.
+
+Function Description
+
+Complete the flippingMatrix function in the editor below.
+flippingMatrix has the following parameters:
+- int matrix[2n][2n]: a 2-dimensional array of integers
+
+
+Return:
+
+- int: the maximum sum possible.
 
 */
 
